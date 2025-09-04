150083b55285871bafd48719eeaa69baa7dc05fb
piyumal1997 <piyumalpriyanga456@gmail.com>
2025-09-04 08:20:46 +0530

    Tourism 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { LocationProvider } from './contexts/LocationContext';
 import AppRouter from './router/AppRouter';
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <LocationProvider>
-      <Router>
         <div className="bg-gray-50 font-sans">
           <AppRouter />
         </div>
-      </Router>
     </LocationProvider>
   );
 }
--- src/components/layout/Layout/Layout.jsx
+++ src/components/layout/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import { useLocation, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   
